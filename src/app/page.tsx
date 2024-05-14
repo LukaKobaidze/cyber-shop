a3449def61db499487329192068e99611bf215c6
LukaKobaidze <lukakobaidze.dev@gmail.com>
2024-05-14 18:19:30 +0400

    Added prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
-import HeroMain from './pageComponents/HeroMain';
-import HeroGrid from './pageComponents/HeroGrid';
-import Categories from './pageComponents/Categories';
-import Products from './pageComponents/Products';
+import HeroMain from "./pageComponents/HeroMain";
+import HeroGrid from "./pageComponents/HeroGrid";
+import Categories from "./pageComponents/Categories";
+import Products from "./pageComponents/Products";
 
 export default function Home() {
-  return (
-    <>
-      <HeroMain />
-      <HeroGrid />
-      <Categories />
-    </>
-  );
+   return (
+      <>
+         <HeroMain />
+         <HeroGrid />
+         <Categories />
+      </>
+   );
 }
